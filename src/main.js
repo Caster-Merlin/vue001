00c64429d05f3f8dd18efc45ab28d4549e72545e
Caster-Merlin <aaaasam@vip.qq.com>
2020-01-03 11:12:05 +0800

    第二天，按照国际惯例，先提交代码，后打卡，晚上加鸡腿，老板请洗脚

--- src/main.js
+++ src/main.js
@@ -5,10 +5,19 @@ import VueRouter from 'vue-router'
 //1.2安装路由
 Vue.use(VueRouter)
 
+//导入格式化时间的插件
+import moment from 'moment'
+//定义全局过滤器
+Vue.filter('dateFormat', function(dataStr, pattern="YYYY-MM-DD HH:mm:ss"){
+    return moment(dataStr).format(pattern)
+})
+
 //2.1导入vue-resource
 import VueResource from 'vue-resource'
 //2.2安装vue-resource
 Vue.use(VueResource)
+//设置请求的根路径
+Vue.http.options.root = 'http://www.liulongbin.top:3005'
 
 //导入App根组件
 import app from './APP.vue'
@@ -18,10 +27,11 @@ import './lib/mui/css/mui.min.css'
 import './lib/mui/css/icons-extra.css'
 
 //按需导入Mint-UI中的组件
-import { Header, Swipe, SwipeItem } from 'mint-ui'
+import { Header, Swipe, SwipeItem, Button } from 'mint-ui'
 Vue.component(Header.name, Header)
-Vue.component(Swipe.name, Swipe);
-Vue.component(SwipeItem.name, SwipeItem);
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
+Vue.component(Button.name, Button)
 
 //1.3导入自己的router.js模块
 import router from './router.js'
