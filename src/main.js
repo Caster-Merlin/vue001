859c54c88185a50d15ea4ba9bf476cffa24fc60f
Caster-Merlin <aaaasam@vip.qq.com>
2020-01-05 10:56:17 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+//这是入口文件
+import Vue from 'vue'
+
+//导入App根组件
+import app from './APP.vue'
+
+//导入MUI的样式
+import './lib/mui/css/mui.min.css'
+
+//按需导入Mint-UI中的组件
+import { Header } from 'mint-ui'
+Vue.component(Header.name, Header)
+
+var vm = new Vue({
+    el: '#app',
+    render: c => c(app)
+})
